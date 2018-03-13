22f8ce515b56812dade5b7dc9d3c81c548cf5ddc
unknown <iantolmay@mweb.co.za>
2018-03-13 20:57:24 +0200

    added canvas

--- kinesiologyFE/src/app/Components/home/home.component.ts
+++ kinesiologyFE/src/app/Components/home/home.component.ts
@@ -15,9 +15,22 @@ export class HomeComponent implements OnInit {
       { name: 'keywords', content: 'Kinesiology Practice, Kinesiology booking, Kinesiology,muscle testing, Kinesiology workshops, Healthy Food, Energy, Spiritual' },
       { name: 'description', content: 'Kinesiology Practice offers a platform of kinesiologists, healthy food and workshops to attend to.' }
     ])
-   }
+  }
 
   ngOnInit() {
+    this.canvas();
+  }
+  canvas() {
+    let canvas = document.querySelector('canvas');    
+    canvas.width = (100 / 100 * innerWidth);
+    canvas.height = (50 / 100 * innerHeight);
+    //canvas.style.backgroundColor = "red";
+
+    let c = canvas.getContext('2d');
+
+    //block    
+    c.fillStyle = 'red';
+    c.fillRect(500, 100, 600, 200);
   }
 
 }
